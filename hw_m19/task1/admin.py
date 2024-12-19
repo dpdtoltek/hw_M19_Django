deb824915f806a8d5d99e019bb1894891c3b09a5
Pavel <dpd2003@gmail.com>
2024-12-19 22:35:55 +0300

    Домашнее задание по теме "Сайт администрирования"

--- hw_m19/task1/admin.py
+++ hw_m19/task1/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('username', 'balance', 'age')
+    list_filter = ('balance', 'age')
+    search_fields = ('username',)
+    list_per_page = 30
+
+    readonly_fields = ('balance',)
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    list_filter = ('title',)
+    search_fields = ('size', 'cost')
+    list_per_page = 20
